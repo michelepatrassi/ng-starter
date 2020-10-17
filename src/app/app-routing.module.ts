6d64c4e4dcf3c0016648f19d08570e7e1068c9d6
michele <michele.patrassi@gmail.com>
2020-10-17 10:17:15 +0200

    lazy module with HMR

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LazyModule } from './lazy/lazy.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'lazy',
+    loadChildren: () => import('./lazy/lazy.module').then((m) => m.LazyModule),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled'
-})],
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabled',
+    }),
+    LazyModule,
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
---
+++ src/app/lazy/lazy.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lazy',
+  template: ` <p>lazy works!</p> `,
+  styles: [],
+})
+export class LazyComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
